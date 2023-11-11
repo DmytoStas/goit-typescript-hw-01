98f2a703990fc8dcba8a98e2b073e2cd8ec204c6
DmytoStas <dima7stas777@gmail.com>
2023-11-11 21:16:41 +0000

    Deploying to gh-pages from @ DmytoStas/goit-typescript-hw-01@150153c7a23eb4826fce6f53a4a98071e273dc86 🚀

---
+++ index.8129040c.js
@@ -0,0 +1,3 @@
+!function(){let e=(e,t)=>{console.log(`${e} ${t}`)},t=document.querySelector("button"),o=document.querySelector("input");t&&o&&t.addEventListener("click",()=>{e(o.value,"hello!")})}();//# sourceMappingURL=index.8129040c.js.map
+
+//# sourceMappingURL=index.8129040c.js.map
---
+++ index.911b8acc.js
@@ -0,0 +1,3 @@
+const e=(e,o)=>{console.log(`${e} ${o}`)},o=document.querySelector("button"),t=document.querySelector("input");o&&t&&o.addEventListener("click",()=>{e(t.value,"hello!")});//# sourceMappingURL=index.911b8acc.js.map
+
+//# sourceMappingURL=index.911b8acc.js.map
